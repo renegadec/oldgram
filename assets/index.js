b26dce7fd8c1d8878a9f0f6f2d69ab6715f985eb
Renegade Confidence <confidencenyirenda@Renegades-MacBook-Air.local>
2025-08-29 11:22:51 +0200

    initial commit

---
+++ assets/index.js
@@ -0,0 +1,30 @@
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "/assets/images/avatar-vangogh.jpg",
+        post: "/assets/images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "/assets/images/avatar-courbet.jpg",
+        post: "/assets/images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "/assets/images/avatar-ducreux.jpg",
+        post: "/assets/images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.',
+  server: {
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+});
+
+
